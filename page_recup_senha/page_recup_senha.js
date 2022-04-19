46a9085e0847d54e9b01714886c61c4fac38050c
GustavoArcoverde <garcoverde3@gmail.com>
2022-04-19 13:23:32 -0300

    cria lógica de verificação de email

---
+++ page_recup_senha/page_recup_senha.js
@@ -0,0 +1,25 @@
+$("#submit").click((event)=>{
+    event.preventDefault()
+    validaEmail()
+})
+
+function validaEmail(){
+    const email = $("#email").val()
+    const emailArr = email.split('')
+
+    try{
+        if(emailArr.indexOf("@") > 3 && email.indexOf("gmail") > email.indexOf("@") && email.indexOf(".com") > email.indexOf("gmail")){
+            $("#msgErroEmail").css('visibility', 'visible')
+            $("#msgErroEmail").text("Email de redefinição de senha enviado.")
+        }else if(emailArr.indexOf("@") > 3 && email.indexOf("outlook") > email.indexOf("@") && email.indexOf(".com") > email.indexOf("outlook")){
+            $("#msgErroEmail").css('visibility', 'visible')
+            $("#msgErroEmail").text("Email de redefinição de senha enviado.")
+        }else{
+            throw new Error("Email Inválido")
+        }
+    }
+    catch(error){
+        $("#msgErroEmail").css('visibility', 'visible')
+        $("#msgErroEmail").text("Email Inválido")
+    }
+}
