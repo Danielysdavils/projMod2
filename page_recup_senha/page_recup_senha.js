5bf4e83f39c751c016fa5c4ed2d7329aadafc1c5
GustavoArcoverde <garcoverde3@gmail.com>
2022-04-25 17:56:30 -0300

    add cor verde a mensagem de email válido

--- page_recup_senha/page_recup_senha.js
+++ page_recup_senha/page_recup_senha.js
@@ -10,9 +10,13 @@ function validaEmail(){
     try{
         if(emailArr.indexOf("@") > 3 && email.indexOf("gmail") > email.indexOf("@") && email.indexOf(".com") > email.indexOf("gmail")){
             $("#msgErroEmail").css('visibility', 'visible')
+            $("#msgErroEmail").css('background-color', 'lightgreen')
+            $("#msgErroEmail").css('color', 'green')
             $("#msgErroEmail").text("Email de redefinição de senha enviado.")
         }else if(emailArr.indexOf("@") > 3 && email.indexOf("outlook") > email.indexOf("@") && email.indexOf(".com") > email.indexOf("outlook")){
             $("#msgErroEmail").css('visibility', 'visible')
+            $("#msgErroEmail").css('background-color', 'lightgreen')
+            $("#msgErroEmail").css('color', 'green')
             $("#msgErroEmail").text("Email de redefinição de senha enviado.")
         }else{
             throw new Error("Email Inválido")
