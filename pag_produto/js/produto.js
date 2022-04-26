378f64118b804e9634a7b771ce729bd59541120f
vgcgabe <v.g.cguedes@gmail.com>
2022-04-26 01:04:26 -0300

    Primeira versão funcional do produto

--- pag_produto/js/produto.js
+++ pag_produto/js/produto.js
@@ -1,6 +1,32 @@
-function criaNovoBloco(){
-    console.log('funciona');
+// function recebeCor(){
+//     let colorPicker = document.querySelector("#colorPicker");
+//     let cor = colorPicker.value;
+//     return cor;
+// }
+
+function criaNovoBlocoVerde(){
+    //let cor = recebeCor();
     const divFilha = document.createElement("textarea");
-    divFilha.innerText = "Oi mamãe!";
+    divFilha.classList.add('botaoVerde');
+    //$('textarea').css({backgroundColor: cor});
+    divFilha.placeholder = "Insira seu texto...";
+    document.querySelector("#divMae").appendChild(divFilha);
+}
+
+function criaNovoBlocoAmarelo(){
+    //let cor = recebeCor();
+    const divFilha = document.createElement("textarea");
+    divFilha.classList.add('botaoAmarelo');
+    //$('textarea').css({backgroundColor: cor});
+    divFilha.placeholder = "Insira seu texto...";
+    document.querySelector("#divMae").appendChild(divFilha);
+}
+
+function criaNovoBlocoVermelho(){
+    //let cor = recebeCor();
+    const divFilha = document.createElement("textarea");
+    divFilha.classList.add('botaoVermelho');
+    //$('textarea').css({backgroundColor: cor});
+    divFilha.placeholder = "Insira seu texto...";
     document.querySelector("#divMae").appendChild(divFilha);
 }
