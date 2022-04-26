892e7ba80ddcaaed8d4a260f07c114352f68ea9c
vgcgabe <v.g.cguedes@gmail.com>
2022-04-26 20:46:54 -0300

    é possível criar post-its de qualquer cor agora

--- pag_produto/js/produto.js
+++ pag_produto/js/produto.js
@@ -1,32 +1,47 @@
-// function recebeCor(){
-//     let colorPicker = document.querySelector("#colorPicker");
-//     let cor = colorPicker.value;
-//     return cor;
+function recebeCor(){
+    let colorPicker = document.querySelector("#colorPicker");
+    let cor = colorPicker.value;
+    return cor;
+}
+
+function criaDiv(cor){
+    const divFilha = document.createElement("div");
+    divFilha.classList.add('divBloco');
+    divFilha.innerHTML = `<textarea style="background-color:${cor};"></textarea>
+                                <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            </div>`;
+    document.querySelector("#divMae").appendChild(divFilha);
+}
+
+function criaNovoBloco(){
+    let cor = recebeCor();
+    criaDiv(cor);
+}
+
+// function criaNovoBlocoVerde(){
+//     //let cor = recebeCor();
+//     const divFilha = document.createElement("textarea");
+//     divFilha.classList.add('botaoVerde');
+//     //$('textarea').css({backgroundColor: cor});
+//     divFilha.placeholder = "Insira seu texto...";
+//     document.querySelector("#divMae").appendChild(divFilha);
 // }
 
-function criaNovoBlocoVerde(){
-    //let cor = recebeCor();
-    const divFilha = document.createElement("textarea");
-    divFilha.classList.add('botaoVerde');
-    //$('textarea').css({backgroundColor: cor});
-    divFilha.placeholder = "Insira seu texto...";
-    document.querySelector("#divMae").appendChild(divFilha);
-}
+// function criaNovoBlocoAmarelo(){
+//     //let cor = recebeCor();
+//     const divFilha = document.createElement("textarea");
+//     divFilha.classList.add('botaoAmarelo');
+//     //$('textarea').css({backgroundColor: cor});
+//     divFilha.placeholder = "Insira seu texto...";
+//     document.querySelector("#divMae").appendChild(divFilha);
+// }
 
-function criaNovoBlocoAmarelo(){
-    //let cor = recebeCor();
-    const divFilha = document.createElement("textarea");
-    divFilha.classList.add('botaoAmarelo');
-    //$('textarea').css({backgroundColor: cor});
-    divFilha.placeholder = "Insira seu texto...";
-    document.querySelector("#divMae").appendChild(divFilha);
-}
-
-function criaNovoBlocoVermelho(){
-    //let cor = recebeCor();
-    const divFilha = document.createElement("textarea");
-    divFilha.classList.add('botaoVermelho');
-    //$('textarea').css({backgroundColor: cor});
-    divFilha.placeholder = "Insira seu texto...";
-    document.querySelector("#divMae").appendChild(divFilha);
-}
+// function criaNovoBlocoVermelho(){
+//     //let cor = recebeCor();
+//     const divFilha = document.createElement("textarea");
+//     divFilha.classList.add('botaoVermelho');
+//     //$('textarea').css({backgroundColor: cor});
+//     divFilha.placeholder = "Insira seu texto...";
+//     document.querySelector("#divMae").appendChild(divFilha);
+// }
