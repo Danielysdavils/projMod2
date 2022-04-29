4e72b3d8f2fc3f6e3e33b0920f7d29feb26ee62c
vgcgabe <v.g.cguedes@gmail.com>
2022-04-29 11:39:31 -0300

    Commit final do produto

--- pag_produto/js/produto.js
+++ pag_produto/js/produto.js
@@ -18,30 +18,3 @@ function criaNovoBloco(){
     let cor = recebeCor();
     criaDiv(cor);
 }
-
-// function criaNovoBlocoVerde(){
-//     //let cor = recebeCor();
-//     const divFilha = document.createElement("textarea");
-//     divFilha.classList.add('botaoVerde');
-//     //$('textarea').css({backgroundColor: cor});
-//     divFilha.placeholder = "Insira seu texto...";
-//     document.querySelector("#divMae").appendChild(divFilha);
-// }
-
-// function criaNovoBlocoAmarelo(){
-//     //let cor = recebeCor();
-//     const divFilha = document.createElement("textarea");
-//     divFilha.classList.add('botaoAmarelo');
-//     //$('textarea').css({backgroundColor: cor});
-//     divFilha.placeholder = "Insira seu texto...";
-//     document.querySelector("#divMae").appendChild(divFilha);
-// }
-
-// function criaNovoBlocoVermelho(){
-//     //let cor = recebeCor();
-//     const divFilha = document.createElement("textarea");
-//     divFilha.classList.add('botaoVermelho');
-//     //$('textarea').css({backgroundColor: cor});
-//     divFilha.placeholder = "Insira seu texto...";
-//     document.querySelector("#divMae").appendChild(divFilha);
-// }
